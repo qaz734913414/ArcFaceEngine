94319ab5ef37a927ae02828493e25e3737055f11
lumo <rivkaer@qq.com>
2020-06-12 10:53:59 +0800

    feature: 增加组件的初始化器

---
+++ src/main/java/com/lumotime/arcface/ArcFaceAppCompat.kt
@@ -0,0 +1,23 @@
+package com.lumotime.arcface
+
+import cn.novakj.j3.core.base.BaseApplication
+import cn.novakj.j3.core.base.ICompatApplication
+
+/**
+ * <p>文件名称: ArcFaceAppCompat </p>
+ * <p>所属包名: com.lumotime.arcface</p>
+ * <p>描述: 用于维护ArcFaceAppCompat状态的实例 </p>
+ * <p>创建时间: 2020/6/12 10:44 </p>
+ * @author <a href="mail to: cnrivkaer@outlook.com" rel="nofollow">lumo</a>
+ * @version v1.0
+ */
+class ArcFaceAppCompat : ICompatApplication{
+
+    override fun initModuleApplicationInMainThread(application: BaseApplication?) {
+
+    }
+
+    override fun initModuleApplicationInBackThread(application: BaseApplication?) {
+
+    }
+}
